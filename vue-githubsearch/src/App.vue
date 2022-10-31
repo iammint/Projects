1b17baa9c99741fb5369e9bc76d94adb1be8bd94
Mint <m@haochen.me>
2022-10-31 22:07:00 +0800

    search

---
+++ vue-githubsearch/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <searchComp></searchComp>
+    <listComp></listComp>
+  </div>
+</template>
+
+<script>
+import searchComp from "./components/searchComp.vue"
+import listComp from "./components/listComp.vue"
+export default {
+  components: {
+    searchComp,
+    listComp,
+  },
+}
+</script>
---
+++ vue-githubsearch/src/components/searchComp.vue
@@ -0,0 +1,47 @@
+<template>
+  <section class="jumbotron">
+    <h3 class="jumbotron-heading">Search Github Users</h3>
+    <div>
+      <input
+        type="text"
+        placeholder="Type in the name"
+        v-model="keyWord"
+      />&nbsp;
+      <button @click="searchUsers">Search</button>
+    </div>
+  </section>
+</template>
+
+<script>
+import axios from "axios"
+export default {
+  name: "searchComp",
+  data() {
+    return {
+      keyWord: "",
+    }
+  },
+  methods: {
+    searchUsers() {
+      axios.get(`https://api.github.com/search/users?q=${this.keyWord}`).then(
+        (response) => {
+          console.log("Success", response.data)
+        },
+        (reason) => {
+          console.log("Failure", reason.message)
+        }
+      )
+    },
+  },
+  mounted() {
+    console.log(this)
+  },
+}
+</script>
+
+<style scoped>
+section {
+  text-align: center;
+  margin-top: 20px;
+}
+</style>
